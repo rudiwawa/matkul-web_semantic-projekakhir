b89ff2987729a89a2d544d484f693f8757aa2e08
rudiwawa <rudiwawa1980@gmail.com>
2019-11-26 16:08:13 +0700

    2.0 view validasi pembimbing

--- application/controllers/Akademik.php
+++ application/controllers/Akademik.php
@@ -4,28 +4,24 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class Akademik extends CI_Controller
 {
 
-    /**
-     * Index Page for this controller.
-     *
-     * Maps to the following URL
-     *        http://example.com/index.php/welcome
-     *    - or -
-     *        http://example.com/index.php/welcome/index
-     *    - or -
-     * Since this controller is set as the default controller in
-     * config/routes.php, it's displayed at http://example.com/
-     *
-     * So any other public methods not prefixed with an underscore will
-     * map to /index.php/welcome/<method_name>
-     * @see https://codeigniter.com/user_guide/general/urls.html
-     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('M_akademik');
+        $this->date = new DateTime();
+    }
+
     public function index(Type $var = null)
     {
         $this->ValidasiPembimbing();
     }
     public function ValidasiPembimbing()
     {
+        $res = $this->M_akademik->get_ValidasiPembimbing();
+        $res = $this->groub_by_kelompok('kelompok', $res);
+        // var_dump ($res);
         $data = array(
+            'data' => $res,
             'sidebar'=>'Sidebar_akademik',
             'content' => 'ValidasiPembimbing',
         );
@@ -38,5 +34,36 @@ class Akademik extends CI_Controller
             'content' => 'ValidasiSemhasPkl',
         );
         $this->load->view('index', $data);
+    }
+    function groub_by_kelompok($by, $data)
+    {
+        $tmp = array();
+        // $tmp[]['value'] = array();
+        $c = -1;
+        // $tmp[]['value'] = array();
+        foreach ($data as $key => $value) {
+            $trim = "http://www.semanticweb.org/kharis/ontologies/2019/10/pkl-filkom#";
+            $found = false;
+            foreach ($tmp as $key => $tmp2) {
+                if ($value[$by]['value'] == $tmp2['kelompok']) {
+                    $found = true;
+                }
+            }
+            // echo "found".$found."<br>";
+            if (!$found) {
+                $tmp_id = $value[$by]['value'];
+                array_push($tmp, ['kelompok' => $tmp_id]);
+                $c++;
+                // echo "c=$c";
+                array_push($tmp[$c], ['data' => $value]);
+            } else {
+                // $swith=true;
+                // echo "KETEMU";
+                array_push($tmp[$c], ['data' => $value]);
+            }
+        }
+        return $tmp;
+        // var_dump($c);
+        // var_dump($tmp);
     }
 }
--- application/views/ValidasiPembimbing.php
+++ application/views/ValidasiPembimbing.php
@@ -1,134 +1,130 @@
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Data Mahasiswa dan Dosen Pembimbing PKL</h3>
 
-      <!-- Main content -->
-      <section class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">Data Mahasiswa dan Dosen Pembimbing PKL</h3>
+            <div class="card-tools">
+              <div class="input-group input-group-sm" style="width: 150px;">
+                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
-                  <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                      <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-
-                      <div class="input-group-append">
-                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                      </div>
-                    </div>
-                  </div>
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
                 </div>
-                <!-- /.card-header -->
-                <div class="card-body table-responsive p-0">
-                  <table class="table table-hover">
-                    <thead>
-                      <tr>
-                        <th>Kelompok</th>
-                        <th>Nama Anggota</th>
-                        <th>Tempat PKL</th>
-                        <th>Keminatan</th>
-                        <th>Dosen Pembimbing</th>
-                        <th>Validasi</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Hanifah , Kharis, Danang, Dicky</td>
-                        <td>Gojek</td>
-                        <td><span class="tag tag-success">KC</span></td>
-                        <td>Dosen KC A</td>
-                        <td><a href="#" class="btn btn-success btn-lg">Validasi</a></td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Hanifah2 , Kharis2, Danang2, Dicky2</td>
-                        <td>Gojek</td>
-                        <td><span class="tag tag-success">MGM</span></td>
-                        <td>Dosen MGM B</td>
-                        <td><a href="#" class="btn btn-success btn-lg">Validasi</a></td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>Hanifah3 , Kharis3, Danang3, Dicky3</td>
-                        <td>Gojek</td>
-                        <td><span class="tag tag-success">RPL</span></td>
-                        <td>Dosen RPL A</td>
-                        <td><a href="#" class="btn btn-success btn-lg">Validasi</a></td>
-                      </tr>
-                      <tr>
-                        <td>4</td>
-                        <td>Hanifah4 , Kharis4, Danang4</td>
-                        <td>Gojek</td>
-                        <td><span class="tag tag-success">JARINGAN</span></td>
-                        <td>Dosen JARINGAN D</td>
-                        <td><a href="#" class="btn btn-success btn-lg">Validasi</a></td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.card-body -->
               </div>
-              <!-- /.card -->
             </div>
           </div>
-          <!-- /.row -->
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0">
+            <table class="table table-hover">
+              <thead>
+                <tr>
+                  <th>Kelompok</th>
+                  <th>Nama Anggota</th>
+                  <th>Tempat PKL</th>
+                  <th>Keminatan</th>
+                  <th>Dosen Pembimbing</th>
+                  <th>Validasi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                // var_dump($data);
+                $trim = "http://www.semanticweb.org/kharis/ontologies/2019/10/pkl-filkom#";
+                foreach ($data as $key => $kelompok) {
+                  // echo $key;
+                  ?>
+                  <tr>
+                    <td><?= substr($kelompok['kelompok'], 64); ?></td>
+                    <td class="list-inline-item">
+                      <?php
+                        foreach ($kelompok as $key => $value) {
+                          if (!($key === "kelompok")) {
+                            ?>
+
+                          <?= $value['data']['nama']['value'] ?>,
+
+
+                      <?php }
+                        }
+                        ?>
+                    </td>
+                    <td><?= ($kelompok[0]['data']['tempat_pkl']['value']); ?></td>
+                    <td><span class="tag tag-success"><?= ($kelompok[0]['data']['minat']['value']); ?></span></td>
+                    <td><?= substr($kelompok[0]['data']['dosen']['value'],64); ?></td>
+                    <td><a href="#" class="btn btn-success btn-lg">Validasi</a></td>
+                  </tr>
+                <?php
+                }
+
+                ?>
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+    </div>
+    <!-- /.row -->
 
 
 
 
 
 
-          <!-- /.row -->
-          <div class="row" text-center>
-            <div class="col-12">
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">Hasil Validasi PKL</h3>
+    <!-- /.row -->
+    <div class="row" text-center>
+      <div class="col-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Hasil Validasi PKL</h3>
 
-                  <div class="card-tools">
-                    <div class="input-group input-group-sm" style="width: 150px;">
-                      <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+            <div class="card-tools">
+              <div class="input-group input-group-sm" style="width: 150px;">
+                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
 
-                      <div class="input-group-append">
-                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                      </div>
-                    </div>
-                  </div>
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
                 </div>
-                <!-- /.card-header -->
-                <div class="card-body table-responsive p-0" style="height: 300px;">
-                  <table class="table table-head-fixed">
-                    <thead>
-                      <tr>
-                        <th>Kelompok</th>
-                        <th>Nama Anggota</th>
-                        <th>Tempat PKL</th>
-                        <th>Keminatan</th>
-                        <th>Dosen Pembimbing</th>
-                        <th>Hasil Validasi</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Hanifah , Kharis, Danang, Dicky</td>
-                        <td>Gojek</td>
-                        <td><span class="tag tag-success">KC</span></td>
-                        <td>Dosen KC A</td>
-                        <td>Tervalidasi</td>
-                      </tr>
-
-                    </tbody>
-                  </table>
-                </div>
-                <!-- /.card-body -->
               </div>
-              <!-- /.card -->
             </div>
           </div>
-          <!-- /.row -->
-          <!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </section>
-     
+          <!-- /.card-header -->
+          <div class="card-body table-responsive p-0" style="height: 300px;">
+            <table class="table table-head-fixed">
+              <thead>
+                <tr>
+                  <th>Kelompok</th>
+                  <th>Nama Anggota</th>
+                  <th>Tempat PKL</th>
+                  <th>Keminatan</th>
+                  <th>Dosen Pembimbing</th>
+                  <th>Hasil Validasi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Hanifah , Kharis, Danang, Dicky</td>
+                  <td>Gojek</td>
+                  <td><span class="tag tag-success">KC</span></td>
+                  <td>Dosen KC A</td>
+                  <td>Tervalidasi</td>
+                </tr>
+
+              </tbody>
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+    </div>
+    <!-- /.row -->
+    <!-- /.row -->
+  </div><!-- /.container-fluid -->
+</section>
